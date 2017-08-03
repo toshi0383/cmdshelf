ffa31fdb02061767f550144394bc68a33c424df6
toshi0383 <t.suzuki326@gmail.com>
2017-08-03 14:00:56 +0900

    Pass through stdin

--- Sources/cmdshelf/Functions.swift
+++ Sources/cmdshelf/Functions.swift
@@ -13,14 +13,20 @@ func safeShellOutAndPrint(to: String, arguments: [String] = []) {
 }
 
 func shellOutAndPrint(to: String, arguments: [String] = []) throws {
-    let stdoutHandler: (FileHandle) -> Void = { handler in
-        let data = handler.availableData
-        if let str = String(data: data, encoding: .utf8) {
-            queuedPrint(str)
+    let process = Process()
+    process.launchPath = "/bin/bash"
+    process.arguments = ["-c", "\(to) \(arguments.joined(separator: " "))"]
+    let inPipe = Pipe()
+    inPipe.fileHandleForWriting.writeabilityHandler = { handler in
+        if let d = readLine(strippingNewline: false)?.data(using: .utf8) {
+            handler.write(d)
+        } else {
+            handler.closeFile()
         }
     }
-    let stdout = Pipe()
-    stdout.fileHandleForReading.readabilityHandler = stdoutHandler
-    try shellOut(to: to, arguments: arguments, outputHandle: stdout.fileHandleForWriting)
-    queuedPrintln("")
+
+    process.standardInput = inPipe
+    process.launch()
+    process.waitUntilExit()
+    inPipe.fileHandleForWriting.writeabilityHandler = nil
 }
