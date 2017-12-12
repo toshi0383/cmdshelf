a0685f7225e0db08a506131d806bd609d86d027d
toshi0383 <t.suzuki326@gmail.com>
2017-12-13 02:34:31 +0900

    Use toshi0383/Commander to remove Spectre from dependencies

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     name: "cmdshelf",
     dependencies: {
         let deps: [Package.Dependency] = [
-            .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0),
+            .Package(url: "https://github.com/toshi0383/Commander.git", majorVersion: 0),
             .Package(url: "https://github.com/toshi0383/PathKit.git", majorVersion: 0),
             .Package(url: "https://github.com/toshi0383/Reporter.git", majorVersion: 0),
             .Package(url: "https://github.com/jpsim/Yams.git", majorVersion: 0)
