96e49b36276f4ddc43ee5345bcf8e4b7584c04ea
toshi0383 <t.suzuki326@gmail.com>
2018-06-26 15:00:20 +0900

    Add bash-completion file

---
+++ Sources/Scripts/cmdshelf-completion.bash
@@ -0,0 +1,39 @@
+# Bash completion script for cmdshelf.
+
+_cmdshelf() {
+    local commands command cur prev
+    commands="blob cat list ls remote run update"
+
+    command="${COMP_WORDS[1]}"
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    if [ $COMP_CWORD == 1 ]
+    then
+        COMPREPLY=($(compgen -W "${commands}" -- ${cur}))
+        return 0
+    fi
+
+    if [ $COMP_CWORD == 2 -a "${command}" == help ]
+    then
+        COMPREPLY=($(compgen -W "${commands}" -- ${cur}))
+        return 0
+    fi
+
+    case "${command}" in
+        blob|remote)
+            COMPREPLY=($(compgen -W 'add list remove' -- ${cur}))
+            return 0
+            ;;
+        list|ls)
+            COMPREPLY=($(compgen -W '--path' -- ${cur}))
+            return 0
+            ;;
+        run)
+            COMPREPLY=($(compgen -W "$(cmdshelf list)" -- ${cur}))
+            return 0
+            ;;
+    esac
+}
+
+complete -F _cmdshelf cmdshelf
