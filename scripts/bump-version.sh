d5cd21cc2cd79c05023ef5b4c14e20c04a911059
toshi0383 <t.suzuki326@gmail.com>
2018-09-03 05:58:36 +0900

    Update bump script

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -3,3 +3,5 @@ VERSION=${1:?}
 CURRENT=${2:-`cmdshelf --version`}
 sed -i "" -e "s/master/${VERSION}/" CHANGELOG.md
 git grep -l $CURRENT | grep -v CHANGELOG.md | xargs sed -i "" -e "s/${CURRENT}/${VERSION}/g"
+git commit -am "$VERSION"
+git tag $VERSION
