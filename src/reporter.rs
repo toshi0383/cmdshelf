02fc9c300feb74ec9491b62204467c2e059abd50
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2018-09-11 00:41:04 +0900

    Add a test for reporter::_println_with()

--- src/reporter.rs
+++ src/reporter.rs
@@ -37,3 +37,36 @@ pub fn error(message: String) {
 pub fn warning(message: String) {
     _println(&mut ::std::io::stdout(), Color::Yellow, message);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{Color, _println};
+    
+    #[test]
+    fn _println_with_color_red() {
+        let mut v: Vec<u8> = Vec::new();
+        _println(&mut v, Color::Red, "__test__".to_string());
+        assert_eq!(String::from_utf8(v).unwrap(), "\u{1b}[0;31m__test__\u{1b}[0;0m\n");
+    }
+
+    #[test]
+    fn _println_with_color_green() {
+        let mut v: Vec<u8> = Vec::new();
+        _println(&mut v, Color::Green, "__test__".to_string());
+        assert_eq!(String::from_utf8(v).unwrap(), "\u{1b}[0;32m__test__\u{1b}[0;0m\n");
+    }
+
+    #[test]
+    fn _println_with_color_yellow() {
+        let mut v: Vec<u8> = Vec::new();
+        _println(&mut v, Color::Yellow, "__test__".to_string());
+        assert_eq!(String::from_utf8(v).unwrap(), "\u{1b}[0;33m__test__\u{1b}[0;0m\n");
+    }
+
+    #[test]
+    fn _println_with_color_reset() {
+        let mut v: Vec<u8> = Vec::new();
+        _println(&mut v, Color::Reset, "__test__".to_string());
+        assert_eq!(String::from_utf8(v).unwrap(), "\u{1b}[0;0m__test__\u{1b}[0;0m\n");
+    }
+}
